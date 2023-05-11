a1fcf7c6f03fa5ac115a0d73379e223257a8c43d
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-11 19:50:35 +0200

    excursive include template files

--- MarkdownExport.py
+++ MarkdownExport.py
@@ -14,6 +14,9 @@ def load_utf8(filename):
     with codecs.open(filename, 'r', encoding='utf-8') as f:
         return f.read()
 
+def remove_values_from_list(the_list, val):
+   return [value for value in the_list if value != val]
+
 class markdown_export_command(sublime_plugin.TextCommand):
     def run(self, edit):
         try:
@@ -40,15 +43,18 @@ class markdown_export_command(sublime_plugin.TextCommand):
                 out = out.replace("<%= TITLE =%>", os.path.splitext(os.path.basename(md_path))[0])
             if 'HTML' in template_insert:
                 out = out.replace("<%= HTML =%>", markdown(md))
-            for item in template_load:
-                item_file_name = settings.get("templates").get(template_inuse).get(item)
-                item_file = load_utf8(package_path + "templates/" + item_file_name)
-                out = out.replace("<%- " + item + " -%>", item_file)
+            while not template_load == []:
+                for item in template_load:
+                    item_file_name = settings.get("templates").get(template_inuse).get(item)
+                    item_file = load_utf8(package_path + "templates/" + item_file_name)
+                    out = out.replace("<%- " + item + " -%>", item_file)
+                    template_load = remove_values_from_list(template_load, item)
+                    template_load.extend(re.findall('<%- (.*?) -%>', out, re.DOTALL))
 
             out_path = os.path.splitext(md_path)[0] + '.html'
             save_utf8(out_path, out)
 
             webbrowser.open("file://" + out_path)
-            
+
         except Exception as e:
             sublime.error_message("Error in MarkdownExport package:\n\n" + str(e))
